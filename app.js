fd36f2022bb186c38823aae9fbc3b3b7e83aeef6
Rachad-4 <rachadneyland@gmail.com>
2024-02-05 20:19:32 -0600

    Add ability to use the physical keyboard
    Users can interact with the calculator using thier keyboard.

--- app.js
+++ app.js
@@ -113,14 +113,130 @@ for (let i = 0; i < document.querySelectorAll(`.key`).length; i++){
 
             default:
                 if (cur.length < 25){
-                    console.log(this.innerHTML);
                     cur += this.innerHTML;
                     displayScreen.textContent = cur;
-                    console.log(cur);
                 }      
         } 
     })
 }
+
+document.addEventListener(`keydown`, function(event){
+
+    console.log(event.key);
+
+    if (event.key >= 0 && event.key <= 9) {
+        if (cur.length < 25){
+            cur += event.key;
+            displayScreen.textContent = cur;
+        } 
+    } else {
+        switch(event.key){
+            case "-":
+                if (firstEntry){
+                    prev = cur; 
+                    cur = "";
+                    operator = "-";
+                    firstEntry = false;
+                    isDecimal = false 
+                }else {
+                    cur = operation(prev,operator,cur);
+                    operator = event.key;
+                    displayScreen.innerHTML = cur;
+                    prev = cur;
+                    cur = ""; 
+                    isDecimal = false 
+                    break;
+                }
+
+            case "+":
+                if (firstEntry){
+                    prev = cur; 
+                    cur = "";
+                    operator = "-";
+                    firstEntry = false; 
+                    isDecimal = false
+                }else {
+                    cur = operation(prev,operator,cur);
+                    operator = event.key;
+                    displayScreen.innerHTML = cur;
+                    prev = cur;
+                    cur = "";    
+                    isDecimal = false                
+                    break;
+                }
+
+            case "*":
+                if (firstEntry){
+                    prev = cur; 
+                    cur = "";
+                    operator = "-";
+                    firstEntry = false; 
+                    isDecimal = false
+                }else {
+                    cur = operation(prev,operator,cur);
+                    operator = event.key
+                    displayScreen.innerHTML = cur;
+                    prev = cur;
+                    cur = "";       
+                    isDecimal = false             
+                    break;
+                }
+    
+            case "/":
+                if (firstEntry){
+                    prev = cur; 
+                    cur = "";
+                    operator = "-";
+                    firstEntry = false; 
+                    isDecimal = false
+                }else {
+                    cur = operation(prev,operator,cur);
+                    operator = event.key;
+                    displayScreen.innerHTML = cur;
+                    prev = cur;
+                    cur = "";   
+                    isDecimal = false                 
+                    break;
+                }
+    
+            case "AC":
+                clear();
+                break;
+    
+            case "+/-":
+                cur *= -1;
+                displayScreen.innerHTML = cur;
+                break;
+    
+            case "=":
+                cur = operation(prev,operator,cur);
+                displayScreen.innerHTML = cur;
+                prev = cur;
+                cur = ""; 
+                isDecimal = false
+                break;
+
+            case "%":
+                isDecimal = true;
+                cur /= 100;
+                displayScreen.innerHTML = cur;
+                break;
+                
+            case ".":
+                if (isDecimal == false){
+                    isDecimal = true;
+                }else {
+                    break;
+                }
+
+            default:
+                if (cur.length < 25 && isNaN(parseInt(cur))){
+                    cur += event.key;
+                    displayScreen.textContent = cur;
+                }      
+        } 
+    }
+})
 
 soundBtn.addEventListener(`click`, ()=>{
     btnSounds = !btnSounds;
